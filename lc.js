bcd55f8b29e64710d6d30c880b25a24093d97af0
lcofili <lcofili2000@gmail.com>
2021-05-17 10:03:38 +0100

    js assignment

---
+++ lc.js
@@ -0,0 +1,69 @@
+//FUNCTION SUBTRACTION
+
+function sub() {
+var m = 15
+var n = 10
+return m-n
+
+}
+console.log(sub())
+alert(sub())
+    
+
+//FUNCTION MULTIPLICATION
+
+function mul() {
+let e = 8
+let f = 4
+return e*f
+
+}
+console.log(mul())
+alert(mul())
+
+//FUNCTION  CAPS
+
+const CAPS=() => {
+var input= prompt("caps lock")
+var outcome = input.toUpperCase()
+return outcome
+}
+console.log(CAPS())
+alert(CAPS())
+
+
+//FUNCTION  DIVISION
+
+function div() {
+let a = 30
+let b = 5
+return a/b
+}
+console.log(div())
+alert(div())
+
+
+//FUNCTION   
+
+function sqrt() {
+return Number (prompt("number")) **(1/2)
+}
+console.log(sqrt())
+alert(sqrt())    
+
+//FUNCTION ODD OR EVEN NUMBER
+
+    function check () {
+        
+    num = prompt("check for odd or even number")
+    num1 = num%2
+    num2 = num1 == 0? "even" : "odd"
+    return num2
+    }
+    console.log(check())
+    alert(check()) 
+
+
+
+
+// //
